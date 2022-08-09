04785ee3383fbaa84d6b183b2fbd03a0f46379d0
Brahian Sanchez <brahians0701@gmail.com>
2022-08-08 22:46:58 -0500

    Modal Register

--- components/Login/index.js
+++ components/Login/index.js
@@ -1,57 +1,72 @@
-import { motion } from 'framer-motion';
+import { useState } from 'react';
 
-const Login = () => (
-    <>
-        <motion.div
-            className="flex flex-col"
-            animate={{ x: [-10, 0], }}
-            transition={{
-                delay: 0.5,
-                x: { duration: 1 },
-                default: { ease: "linear" }
-            }}
-        >
-            <div className="pb-5">
-                <input
-                    type="text"
-                    placeholder="Username"
-                    className="p-2 rounded-xl text-white bg-indigo-700 shadow-lg outline-0 placeholder:italic placeholder:text-white"
-                />
-            </div>
-            <div>
-                <input
-                    type="password"
-                    placeholder="Password"
-                    className="p-2 rounded-xl text-white bg-indigo-700 shadow-lg outline-0 placeholder:italic placeholder:text-white"
-                />
-            </div>
-        </motion.div>
-        <motion.div
-            className="flex flex-row justify-center pt-5"
-            animate={{ x: [10, 0], }}
-            transition={{
-                delay: 0.5,
-                x: { duration: 1 },
-                default: { ease: "linear" }
-            }}
-        >
-            <div className="mr-2">
-                <button
-                    className="p-2 w-20 italic text-center text-white rounded-lg shadow-lg border-solid border-2 border-indigo-700"
-                >
-                    Login
-                </button>
-            </div>
-            <div>
-                <button
-                    className="p-2 w-20 italic text-center text-white rounded-lg shadow-lg border-solid border-2 border-indigo-700"
-                >
-                    Register
-                </button>
-            </div>
-        </motion.div>
-    </>
+import { motion, AnimatePresence } from 'framer-motion';
+import Modal from '../Modal';
 
-)
+const Login = () => {
+
+    const [modalOpen, setModalOpen] = useState(false)
+
+    const close = () => setModalOpen(false)
+    const open = () => setModalOpen(true)
+
+    return (
+        <>
+            <div
+                className="flex flex-col"
+            >
+                <div className="pb-5">
+                    <motion.input
+                        type="text"
+                        placeholder="Username"
+                        className="p-2 rounded-xl text-white bg-indigo-700 shadow-lg outline-0 placeholder:italic placeholder:text-white"
+                        whileHover={{ scale: 1.1 }}
+                        whileTap={{ scale: 0.9 }}
+                    />
+                </div>
+                <div>
+                    <motion.input
+                        type="password"
+                        placeholder="Password"
+                        className="p-2 rounded-xl text-white bg-indigo-700 shadow-lg outline-0 placeholder:italic placeholder:text-white"
+                        whileHover={{ scale: 1.1 }}
+                        whileTap={{ scale: 0.9 }}
+                    />
+                </div>
+            </div>
+            <div
+                className="flex flex-row justify-center pt-5"
+            >
+                <div className="mr-2">
+                    <motion.button
+                        className="p-2 w-20 italic text-center text-white rounded-lg shadow-lg border-solid border-2 border-indigo-700"
+                        whileHover={{ scale: 1.1 }}
+                        whileTap={{ scale: 0.9 }}
+                    >
+                        Login
+                    </motion.button>
+                </div>
+                <div>
+                    <motion.button
+                        className="p-2 w-20 italic text-center text-white rounded-lg shadow-lg border-solid border-2 border-indigo-700"
+                        whileHover={{ scale: 1.1 }}
+                        whileTap={{ scale: 0.9 }}
+                        onClick={ () => (modalOpen ? close() : open()) }
+                    >
+                        Register
+                    </motion.button>
+                </div>
+            </div>
+
+            <AnimatePresence
+                initial={ false }
+                exitBeforeEnter={ true }
+                onExitComplete={ () => null }
+            >
+                { modalOpen && <Modal modalOpen={ modalOpen } handleClose={ close } /> }
+            </AnimatePresence>
+        </>
+    )
+}
 
 export default Login
